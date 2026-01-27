c4b8818cf52340c11dc0c76123e4c5eb90df4049
cuongccna <cuong.ccna@yahoo.com.vn>
2026-01-28 00:07:52 +0700

    updae deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,10 @@ module.exports = {
     {
       name: "coin87-backend",
       cwd: "./backend",
-      script: "venv/bin/uvicorn",
-      args: "app.main:app --host 0.0.0.0 --port 9000",
+      script: "venv/bin/python",
+      args: "-m uvicorn app.main:app --host 0.0.0.0 --port 9000",
       env: {
-        PYTHONPATH: ".." // Set pythonpath to include backend directory
+        PYTHONPATH: "."
       }
     },
     {
